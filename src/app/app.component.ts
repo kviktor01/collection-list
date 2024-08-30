c00af48c6a904bbc943a05e0818f7312811b276c
katonaviktorzoltan <viktor.zoltan.katona@lechnerkozpont.hu>
2024-08-30 10:17:33 +0200

    add firebase and primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'collection-list';
+
+  constructor() {
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {getAuth, provideAuth} from '@angular/fire/auth';
+import {getFirestore, provideFirestore} from '@angular/fire/firestore';
+import {getDatabase, provideDatabase} from '@angular/fire/database';
 
 @NgModule({
   declarations: [
@@ -12,7 +16,21 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideFirebaseApp(() => initializeApp({
+      projectId: 'collection-list-2a7ba',
+      appId: '1:511129198833:web:74df114a3c969432f18950',
+      databaseURL: 'https://collection-list-2a7ba-default-rtdb.europe-west1.firebasedatabase.app',
+      storageBucket: 'collection-list-2a7ba.appspot.com',
+      apiKey: 'AIzaSyBzFTaViFjbAiDssDPu1eRQUJO_NjB-yVs',
+      authDomain: 'collection-list-2a7ba.firebaseapp.com',
+      messagingSenderId: '511129198833'
+    })),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase())
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
